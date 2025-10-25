33e5cbbe0ae049982c18518cce32cf8fc31b8de9
nur alam joy <ajnuralam@gmail.com>
2025-10-25 21:33:15 +0600

    hero card done

--- src/Components/Hero/Card.jsx
+++ src/Components/Hero/Card.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import vector1 from "../../asset/vector1.png";
 
-const Card = ({backgroundColor}) => {
+
+const Card = ({ backgroundColor = "bg-blue-200" }) => {
   return (
-    <div className="w-[100%]">
-      <div
-        className={`card ${backgroundColor}  h-[200px] flex flex-col items-center justify-center space-y-4 `}
-      >
-        <h3 className='text-[20px]' >In Progress</h3>
-        <span className='text-[32px] bold' >0000</span>
+    <div className={`w-full ${backgroundColor} relative rounded-xl h-[200px] flex items-center justify-center overflow-hidden`}>
+      
+      <img
+        src={vector1}
+        alt="vector1"
+        className="leftImg absolute left-0 top-1/2 -translate-y-1/2 w-[50%] h-[100%] object-contain"
+      />
+
+      <img
+        src={vector1}
+        alt="vector2"
+        className="rightImg absolute right-0 top-1/2 -translate-y-1/2 w-[50%] h-[100%] object-contain"
+        style={{ transform: "scaleX(-1)" }}
+      />
+
+      <div className="content relative text-center">
+        <h3 className="text-[20px] font-bold">In Progress</h3>
+        <span className="text-[32px] font-bold">0000</span>
       </div>
     </div>
   );
